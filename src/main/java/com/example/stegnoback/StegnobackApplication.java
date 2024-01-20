e826d385445db24f88e1249982ba0e76ca475676
Tejas <tejas24497@gmail.com>
2024-01-20 14:32:29 +0530

    first

---
+++ src/main/java/com/example/stegnoback/StegnobackApplication.java
@@ -0,0 +1,13 @@
+package com.example.stegnoback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StegnobackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StegnobackApplication.class, args);
+	}
+
+}
